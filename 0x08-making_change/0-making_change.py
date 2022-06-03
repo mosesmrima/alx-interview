43fd5764f536fe2f2e86c7fdc0f7c609b70b9b36
mosesmrima <mrimamss@gmail.com>
2022-06-03 12:06:35 +0300

    readme

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""This module contains one function"""
+
+import sys
+
+def makeChange(coins, total):
+    """ Calculate the least bumber of coins needed to
+    give change.
+    """
+    if total <= 0:  # return 0 if total is less than or equal to zero
+        return 0
+    max_coins = [0] + [sys.maxsize] * (total)
+    for coin in coins:
+        for i in range(coin, total + 1):
+            max_coins[i] = min(max_coins[i], max_coins[i - coin] + 1)
+    if max_coins[-1] != sys.maxsize:
+        return max_coins[-1]
+    else:
+        return -1
